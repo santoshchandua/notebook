9e33bc5cd80c853cfa39ca594da8003926e9b557
santoshchandua <santoshkumarchandua@gmail.com>
2026-09-18 10:47:02 +0530

    Prioritize writing space with collapsible notebook sidebar and tool panel

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='notenest-v9';
+const CACHE='notenest-v10';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('notenest-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
